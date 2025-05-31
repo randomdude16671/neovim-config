1ac314d4ca5a4c433a5e85bca4e2d5d3dbba12d5
randomdude16671 <randomdude16671@proton.me>
2025-05-31 13:41:21 +0530

    Update config

--- lua/ghost/plugins/trouble.lua
+++ lua/ghost/plugins/trouble.lua
@@ -1,27 +1,45 @@
 -- diagnostics in nice little qflist thing
 return {
-	"folke/trouble.nvim",
-	opts = {},
-	keys = {
-		{
-			"<leader>tt",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics (Trouble)",
-		},
-		{
-			"<leader>tb",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>ts",
-			"<cmd>Trouble symbols toggle focus=false<cr>",
-			desc = "Symbols (Trouble)",
-		},
-		{
-			"<leader>tf",
-			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-			desc = "LSP Definitions / references / ... (Trouble)",
-		},
-	},
+  { 
+    "folke/trouble.nvim",
+    opts = {
+    },
+    keys = {
+      {
+        "<leader>tt",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tb",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>ts",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tf",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+    },
+  }, 
+  {
+    "ivanjermakov/troublesum.nvim",
+    config = function()
+      require("troublesum").setup({
+        enabled = true, 
+        autocmd = true,
+        severity_format = {
+          " ",
+          " ",
+          " ", 
+          " ",
+        }, 
+      })
+    end
+  }
 }
