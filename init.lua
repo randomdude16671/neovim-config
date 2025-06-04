77fa9b1d88edc6e2daaf825ab09749b47d74a834
randomdude16671 <randomdude16671@proton.me>
2025-06-04 19:02:23 +0530

    Update configs

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("ghost.core")
-vim.cmd.so("~/.config/nvim/lua/ghost/core/keymaps.lua") -- yea it wasn't working with just require so...
+vim.cmd.so("~/.config/nvim/lua/ghost/core/keymaps.lua")
 
 require("lazy").setup({
     { import = "ghost.plugins" },
-}, require("ghost.plugins.config.lazy"))
+}, require("ghost.core.lazy"))
