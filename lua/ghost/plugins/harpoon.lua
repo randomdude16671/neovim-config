b0037e640ffe2445690d35936e48642797a90778
randomdude16671 <randomdude16671@proton.me>
2025-06-07 19:41:26 +0530

    Update harpoon config

--- lua/ghost/plugins/harpoon.lua
+++ lua/ghost/plugins/harpoon.lua
@@ -4,52 +4,55 @@ return {
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
         config = function()
-            require("harpoon"):setup()
+            require("harpoon").setup()
         end,
         lazy = true,
-        keys = {
-            {
-                "<leader>a",
-                function()
-                    require("harpoon"):list():add()
-                end,
-            },
-            {
-                "<C-e>",
-                function()
-                    require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-                end,
-            },
-            {
-                "<leader>ch",
-                function()
-                    require("harpoon"):list():clear()
-                end,
-            },
-            {
-                "<M-j>",
-                function()
-                    require("harpoon"):list():select(1)
-                end,
-            },
-            {
-                "<M-k>",
-                function()
-                    require("harpoon"):list():select(2)
-                end,
-            },
-            {
-                "<M-l>",
-                function()
-                    require("harpoon"):list():select(3)
-                end,
-            },
-            {
-                "<M-;>",
-                function()
-                    require("harpoon"):list():select(4)
-                end,
-            },
-        },
+        keys = function()
+            local harpoon = require("harpoon")
+            return {
+                {
+                    "<leader>a",
+                    function()
+                        harpoon:list():add()
+                    end,
+                },
+                {
+                    "<C-e>",
+                    function()
+                        harpoon.ui:toggle_quick_menu(harpoon:list())
+                    end,
+                },
+                {
+                    "<leader>ch",
+                    function()
+                        harpoon:list():clear()
+                    end,
+                },
+                {
+                    "<M-j>",
+                    function()
+                        harpoon:list():select(1)
+                    end,
+                },
+                {
+                    "<M-k>",
+                    function()
+                        harpoon:list():select(2)
+                    end,
+                },
+                {
+                    "<M-l>",
+                    function()
+                        harpoon:list():select(3)
+                    end,
+                },
+                {
+                    "<M-;>",
+                    function()
+                        harpoon:list():select(4)
+                    end,
+                },
+            }
+        end,
     },
 }
