ba9be60482a5cefd27314194a3a15355be5d79fe
randomdude16671 <randomdude16671@proton.me>
2025-06-18 23:13:24 +0530

    update snacks.lua

--- lua/ghost/plugins/snacks.lua
+++ lua/ghost/plugins/snacks.lua
@@ -1,18 +1,19 @@
 -- I don't know how to disable icons here :sadge:
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	opts = {
-		input = {
-			enabled = true,
-		},
-		picker = { enabled = true },
-		bigfile = { enabled = true },
-		indent = { enabled = true },
-		dashboard = {
-			enabled = true,
-			preset = {
-				header = [[
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+        input = {
+            enabled = true,
+        },
+        picker = { enabled = true },
+        rename = { enabled = true },
+        bigfile = { enabled = true },
+        indent = { enabled = true },
+        dashboard = {
+            enabled = true,
+            preset = {
+                header = [[
   	 ___      ___ ___
     	|\  \    /  /|\  \
     	\ \  \  /  / | \  \
@@ -21,35 +22,36 @@ return {
     	   \ \__/ /     \ \__\
     		\|__|/       \|__|
 	]],
-			},
-			sections = {
-				{ section = "header" },
-				{ section = "keys",   gap = 1, padding = 1 },
-				{ section = "startup" },
-			},
-		},
-		quickfile = { enabled = true },
-		scope = { enabled = true },
-		notifier = { enabled = true },
-	},
-	keys = {
-		{
-			"<leader>f",
-			function()
-				Snacks.picker.files()
-			end,
-		},
-		{
-			"<leader>g",
-			function()
-				Snacks.picker.grep()
-			end,
-		},
-		{
-			"<C-p>",
-			function()
-				Snacks.picker.buffers()
-			end,
-		},
-	},
+            },
+            sections = {
+                { section = "header" },
+                { section = "keys", gap = 1, padding = 1 },
+                { section = "startup" },
+            },
+        },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        notifier = { enabled = true },
+        terminal = { enabled = true },
+    },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                Snacks.picker.files()
+            end,
+        },
+        {
+            "<leader>g",
+            function()
+                Snacks.picker.grep()
+            end,
+        },
+        {
+            "<C-p>",
+            function()
+                Snacks.picker.buffers()
+            end,
+        },
+    },
 }
