1e34acbdf05272d5cba8adabb55d9a1ed278d8df
randomdude16671 <randomdude16671@proton.me>
2025-07-02 20:40:13 +0530

    stuff

--- lua/ghost/plugins/none-ls.lua
+++ lua/ghost/plugins/none-ls.lua
@@ -4,6 +4,46 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local null_ls = require("null-ls")
+        local custom_diagnostics = {
+            {
+                name = "func-annotation-checker-lua",
+                method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+                filetypes = { "lua" },
+
+                generator = {
+                    fn = function(params)
+                        local diagnostics = {}
+                        for i, line in ipairs(params.content) do
+                            local func_params = line:match("^%s*function%s+[%w%.]+%((.-)%)")
+                            if func_params and func_params:match("%S") then
+                                local previous_line = params.content[i - 1] or ""
+                                if not previous_line:match("^%-%-%-?%s*@param") then
+                                    table.insert(diagnostics, {
+                                        row = i,
+                                        col = 0,
+                                        end_col = #line,
+                                        message = "Ghost: Function with No param types? You sure?",
+                                        severity = 2,
+                                        source = "ghost-lua-checker-subset",
+                                    })
+                                end
+                            end
+                        end
+                        return diagnostics
+                    end,
+                },
+            },
+        }
+
+        for _, diag in ipairs(custom_diagnostics) do
+            null_ls.register({
+                name = diag.name,
+                method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+                filetypes = diag.filetypes,
+                generator = diag.generator,
+            })
+        end
+
         null_ls.setup({
             sources = {
                 -- Formatting:
