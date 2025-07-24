649435ed737d09f7b2507509fe7b3548b95e5085
randomdude16671 <randomdude16671@proton.me>
2025-07-24 19:08:28 +0530

    zk lua

---
+++ lua/ghost/plugins/zk.lua
@@ -0,0 +1,24 @@
+return {
+    "zk-org/zk-nvim",
+    name = "zk",
+    ft = "markdown",
+    config = function()
+        vim.keymap.set("n", "<leader>f", "<cmd>ZkNotes<cr>")
+        require("zk").setup({
+            picker = "snacks_picker",
+            lsp = {
+                config = {
+                    "zk",
+                    cmd = { "zk", "lsp" },
+                    filetypes = "markdown",
+                },
+                auto_attach = { enabled = true },
+            },
+            picker_options = {
+                snacks_picker = {
+                    layout = { preset = "vscode" },
+                },
+            },
+        })
+    end,
+}
