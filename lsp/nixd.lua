701eb7dbb4576b132c10dcbf5da0c4a27a528d63
randomdude16671 <randomdude16671@proton.me>
2025-08-08 17:34:41 +0530

    [lsp-nixd] add nixos and nixpkgs options

--- lsp/nixd.lua
+++ lsp/nixd.lua
@@ -4,5 +4,21 @@ return {
     },
     filetypes = { "nix" },
     root_markers = { "flake.nix", "flake.lock", ".git", "shell.nix", "devenv.nix" },
+    settings = {
+        nixd = {
+            nixpkgs = {
+                -- use system nixpkgs
+                expr = 'import (builtins.getFlake "path:/etc/nixos").inputs.nixpkgs {}',
+            },
+            formatting = {
+                command = { "nix", "fmt" },
+            },
+            options = {
+                nixos = {
+                    expr = '(builtins.getFlake "path:/etc/nixos").nixosConfigurations.hydra.options',
+                },
+            },
+        },
+    },
     single_file_support = true,
 }
