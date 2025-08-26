d4a8d5dd0056d922dddb72e13a05ef9181f38739
Random Dude <randomdude16671@proton.me>
2025-08-26 17:03:26 +0530

    [plugin-addition:flutter-nvim] [autocmds:dart] dart stuff

--- lua/ghost/core/autocmds.lua
+++ lua/ghost/core/autocmds.lua
@@ -31,7 +31,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*.nix",
+    pattern = "nix",
     callback = function()
         vim.opt_local.expandtab = true
         vim.opt_local.shiftwidth = 2
@@ -44,4 +44,14 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     callback = function()
         vim.bo.filetype = "volt"
     end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "dart"
+    callback = function()
+        vim.opt_local.expandtab = true
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.tabstop = 2
+    end, 
 })
