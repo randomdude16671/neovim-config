0764e5bca633b4aa15c0b23f02fc1bbf3a447fee
Random Dude <randomdude16671@proton.me>
2025-09-10 20:15:05 +0530

    [autocmds:netrw-cursorline] update

--- lua/ghost/core/autocmds.lua
+++ lua/ghost/core/autocmds.lua
@@ -30,6 +30,14 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.tabstop = 2
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function()
+        vim.opt_local.cursorlineopt = "both"
+    end,
+})
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "build.volt", "*.volt" },
     callback = function()
