3b52e2b091045ac2917a72d966aba27d733471aa
Random Dude <randomdude16671@proton.me>
2025-09-16 20:55:18 +0530

    [autocmds:remove-cpp-indentation] update config

--- lua/ghost/core/autocmds.lua
+++ lua/ghost/core/autocmds.lua
@@ -31,17 +31,6 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "cpp",
-    callback = function()
-        vim.opt_local.expandtab = true
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.tabstop = 2
-    end,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "netrw",
     callback = function()
