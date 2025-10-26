e311a36eb03c5768e4dd216eada8d0694238ab1b
Random Dude <randomdude16671@proton.me>
2025-10-26 21:55:15 +0530

    ..

--- lua/ghost/core/autocmds.lua
+++ lua/ghost/core/autocmds.lua
@@ -32,6 +32,16 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cpp",
+    callback = function()
+        vim.opt_local.expandtab = true
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.tabstop = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
     pattern = "netrw",
     callback = function()
         vim.opt_local.cursorlineopt = "both"
