84c39652af6b4b3a03e954abce315a8dcc8ddbc4
Random Dude <randomdude16671@proton.me>
2025-10-29 19:10:54 +0530

    debloat

--- lua/ghost/plugins/none-ls.lua
+++ lua/ghost/plugins/none-ls.lua
@@ -57,6 +57,10 @@ return {
 
                 -- Linting:
                 null_ls.builtins.diagnostics.golangci_lint,
+                null_ls.builtins.diagnostics.statix,
+
+                -- Code actions:
+                null_ls.builtins.code_actions.statix,
             },
         })
     end,
